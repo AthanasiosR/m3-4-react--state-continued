86f478026be2130b92ca628b867f11f028b9a905
Athanasios Tommy Rigas <tommyrigas@Athanasioss-MacBook-Pro.local>
2020-11-17 16:58:37 -0500

    Workshop Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import data from "../data";
+import { categories } from "../data";
 
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from "./GlobalStyles";
+import Typeahead from "./Typeahead";
 
 const App = (props) => {
   return (
     <>
       <GlobalStyles />
+      <Wrapper>
+        <Typeahead
+          categories={categories}
+          suggestions={data.books}
+          handleSelect={(suggestion) => {
+            window.alert(suggestion);
+          }}
+        />
+      </Wrapper>
       {/* TODO */}
     </>
   );
 };
+
+const Wrapper = styled.div`
+  /* display: flex;
+  justify-content: center; */
+  margin-top: 25px;
+`;
 
 export default App;
---
+++ src/components/Typeahead.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Suggestion from "./Suggestion";
+
+const Typeahead = ({ suggestions, handleSelect }) => {
+  const [term, setTerm] = useState("");
+  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState(0);
+
+  //filtering the input to return matching titles:
+  let matchingResults = suggestions.filter((book) => {
+    const lowerCasedInput = term.toLowerCase();
+    const lowerCasedTitle = book.title.toLowerCase();
+    const match = lowerCasedTitle.includes(lowerCasedInput);
+    return match && term.length >= 2;
+  });
+
+  let selectedBook = matchingResults[selectedSuggestionIndex];
+
+  return (
+    <>
+      <InputWrapper>
+        <Input
+          type="text"
+          value={term}
+          onChange={(e) => {
+            setTerm(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSelect(selectedBook.title);
+            } else if (
+              e.key === "ArrowUp" &&
+              matchingResults.length > 0 &&
+              selectedSuggestionIndex > 0
+            ) {
+              setSelectedSuggestionIndex(selectedSuggestionIndex - 1);
+            } else if (
+              e.key === "ArrowDown" &&
+              matchingResults.length > 0 &&
+              selectedSuggestionIndex < matchingResults.length - 1
+            ) {
+              setSelectedSuggestionIndex(selectedSuggestionIndex + 1);
+            }
+          }}
+        />
+        <Button onClick={() => setTerm("")}>Clear</Button>
+      </InputWrapper>
+      <SuggestionsWrapper>
+        <Suggestions>
+          {matchingResults.map((book, index) => {
+            const isSelected = index === selectedSuggestionIndex;
+            return (
+              <>
+                <Suggestion
+                  isSelected={isSelected}
+                  key={book.id}
+                  term={term}
+                  filteredList={matchingResults}
+                  index={index}
+                  book={book}
+                  onMouseEnter={() => {
+                    setSelectedSuggestionIndex(index);
+                  }}
+                  onClick={() => {
+                    handleSelect(book.title);
+                  }}
+                />
+              </>
+            );
+          })}
+        </Suggestions>
+      </SuggestionsWrapper>
+    </>
+  );
+};
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  margin-right: 7px;
+  border-radius: 3px;
+  width: 250px;
+`;
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 4px;
+`;
+
+const Suggestions = styled.ul`
+  width: 300px;
+  box-shadow: 0px 9px 28px 0px rgba(0, 0, 0, 0.65);
+  border-radius: 4px;
+`;
+
+const SuggestionsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export default Typeahead;
